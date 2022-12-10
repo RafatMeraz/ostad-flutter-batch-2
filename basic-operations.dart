5a8143ff32c9ddb95abf31a86f9dc47e709d83f9
Rafat Jamader Meraz <razhossain201@gmail.com>
2022-12-11 00:20:33 +0600

    Create basic-operations.dart

---
+++ basic-operations.dart
@@ -0,0 +1,24 @@
+void main() {
+  int a = 53;
+  int b = 34;
+  // addition
+  int c = a + b;
+  print(c);
+  int d = a - b;
+  print(d);
+  int e = a * b;
+  print(e);
+  // type conversion
+  int f = (a / b).toInt();
+  print(f);
+  String age = (24.56 - 1.5).toString();
+  print(age);
+  double result = 23.45 + 3;
+  print(result);
+  
+  
+  // constant
+  const double pi = 3.1415926535897932384626;
+  final double rPi = 3.1415926535897932384626;
+  print(pi);
+}
