6ba4df2187666f4a98b9deb7592e75c3a7fd1a6c
Rafat Jamader Meraz <razhossain201@gmail.com>
2022-12-20 20:24:08 +0600

    Create do-while.dart

---
+++ do-while.dart
@@ -0,0 +1,11 @@
+/// do-while loop
+void main() {
+  int start = 0;
+
+  /// do - while
+  do {
+    print('Print something! $start');
+    start++;
+  } while (start <= 10);
+  print('End of loop');
+}
