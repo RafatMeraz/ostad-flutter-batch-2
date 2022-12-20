d565c020743eacb7cd342a556cef3239838f6e6b
Rafat Jamader Meraz <razhossain201@gmail.com>
2022-12-20 20:21:54 +0600

    Create if-else.dart

---
+++ if-else.dart
@@ -0,0 +1,26 @@
+void main() {
+  int age = 65;
+
+  if (age <= 20) {
+    print('Go to first doctor');
+  }
+  else if (age == 25) {}
+  else if (age == 25) {}
+  else if (age == 25) {}
+  else if (age == 25) {}
+  else if (age == 25) {}
+  else if (age == 25) {}
+  else if (age == 25) {}
+  else if (age == 25) {}
+  else if (age >= 30) {
+    print('Go to second doctor');
+  }
+  else if (age == 60) {
+    print('Go to second doctor');
+  }
+  else {
+    print('Go to third doctor');
+  }
+
+  print('Go to home');
+}
