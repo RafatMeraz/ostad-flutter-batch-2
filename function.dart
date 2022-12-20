b50846ed492e6916a6e0ddf7b39fe360a4cefdbb
Rafat Jamader Meraz <razhossain201@gmail.com>
2022-12-20 20:26:24 +0600

    Create function.dart

---
+++ function.dart
@@ -0,0 +1,21 @@
+main() {
+  printMyName();
+  for (int i=0; i < 1000; i++) {
+    printMyName();
+  }
+  printMyName();
+}
+
+printMyName() {
+  print('My name is Omar');
+  print('My age is 13');
+}
+
+biyerFunction() {
+  print('Biye te gelam');
+  print('Gift dilam');
+  print('Met korlam');
+  print('Lunch');
+  print('bashay chole ashsi');
+}
+
